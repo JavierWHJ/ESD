5f5d027a4551bf7f93c73e14dde498e9491f6236
junxiang <ongjx9@hotmail.com>
2020-03-28 21:09:04 +0800

    Update appointment.js

--- frontend/assets/js/appointment.js
+++ frontend/assets/js/appointment.js
@@ -20,39 +20,31 @@ function onReady(){
         function showPosition(position) {
             var pos = {lat:position.coords.latitude, long:position.coords.longitude}
             function showError(message) {
-                alert(message);
+                alert(message)
             }
-            if (element.innerText != "Appointment Started"){
-                $(async() => {
-                    // change url to localhost:8000/api/v1/start_booking
-                    // var serviceURL = 'http://localhost:5005/start_booking';
-                    serviceURL = 'http://localhost:8000/api/v1/start_booking';
-                    try {
-                        const response =
-                        fetch(
-                            serviceURL,
-                            {
-                                method: "POST",
-                                headers: {"Content-Type" : "application/json"},
-                                mode: "no-cors",
-                                body: JSON.stringify({
-                                    bookingID: bid,
-                                    status: "started",
-                                    location: pos
-                                })
-                            }
-                        );
-                        element.innerText = "Appointment Started"
-                    } catch (error) {
-                        // Errors when calling the service; such as network error, 
-                        // service offline, etc
-                        showError
-                        ('Failed to start appointment, please try again');                
-                    } 
-                });
-            }else{
-                console.log("already started")
-            }
+            
+            $(async() => {
+                serviceURL = 'http://localhost:8000/api/v1/start_booking';
+                try{
+                    const response =
+                    await fetch(
+                        serviceURL,
+                        {
+                            method: "POST",
+                            headers: {"Content-Type" : "application/json"},
+                            mode: 'no-cors',
+                            body: JSON.stringify({
+                                bookingID: bid,
+                                status: "started",
+                                location: pos
+                            })
+                        })
+                }catch (error){
+                    showError('h');
+                }finally{
+                    window.location.reload();
+                }
+            });
         }
-    })		
+    })
 }
