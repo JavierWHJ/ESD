7c514139eeea370b4dd733efca2cb2cc92de734f
junxiang <ongjx9@hotmail.com>
2020-03-29 22:44:02 +0800

    notification now has sender receiver and timestamp

--- api/notification/notification.py
+++ api/notification/notification.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 from flask_cors import CORS
 import json
 from os import environ
+import datetime
 app = Flask(__name__)
 
 
@@ -16,16 +18,20 @@ CORS(app)
 class Notification(db.Model):
     __tablename__ = 'notifications'
     nid = db.Column(db.Integer, primary_key=True)
-    userid = db.Column(db.String(2), nullable=False)
+    sender = db.Column(db.String(2), nullable=False)
+    receiver = db.Column(db.String(2), nullable=False)
     message = db.Column(db.String(500), nullable=False)
+    timestamp = db.Column(db.DateTime(timezone=True), nullable=True)
 
-    def __init__(self, nid, userid, message):
+    def __init__(self, nid, sender, receiver, message):
         self.nid = nid
-        self.userid = userid
+        self.sender = sender
+        self.receiver = receiver
         self.message = message
+        self.timestamp = func.now()
 
     def json(self):
-        return {"nid": self.nid, "userid": self.userid, "message": self.message}
+        return {"nid": self.nid, "sender": self.sender, "receiver": self.receiver, "message": self.message, "timestamp": self.timestamp}
 
 
 @app.route('/notifications')
@@ -52,9 +58,12 @@ def add_notif():
 
 
 @app.route('/notifications/userid=<string:userid>')
-def get_notif_by_userid(userid):
-    notification = Notification.query.filter_by(userid=userid).first()
+def get_notif_by_receiver(userid):
+    notification = Notification.query.filter_by(receiver=userid).first()
+    if notification == None:
+        return jsonify({}), 200
     return jsonify(notification.json()), 200
+
 
 
 @app.route('/notifications/nid=<int:nid>', methods=['DELETE'])
